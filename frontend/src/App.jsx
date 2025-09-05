73093a6bca9a6f2a28034368e83a012a81cbb1a9
Anuj Prakash Dixit <anujprakashdixit@gmail.com>
2025-09-05 18:41:59 +0530

    Added Join Functionality

---
+++ frontend/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+import './App.css';
+import io from 'socket.io-client';
+
+const socket = io("http://localhost:5000");
+
+const App = () => {
+  const [joined, setJoined] = useState(false);
+  const [roomId, setRoomId] = useState("");
+  const [userName, setUserName] = useState("");
+
+  const joinRoom = () => {
+    if(userName && roomId){
+      socket.emit("join", {roomId, userName})
+      setJoined(true);
+    }
+  }
+
+  if (!joined) {
+    return <div className='join-container'>
+      <div className="join-form">
+        <h1>Join Co-Code</h1>
+        <input type="text" placeholder='Enter the Room ID' value={roomId} onChange={(e) => setRoomId(e.target.value)}/>
+
+         <input type="text" placeholder='Enter Your Name' value={userName} onChange={(e) => setUserName(e.target.value)}/>
+
+         <button onClick={joinRoom}>Join Room</button>
+      </div>
+    </div>
+  }
+
+  return <di>App Joined</di>
+}
+
+export default App
